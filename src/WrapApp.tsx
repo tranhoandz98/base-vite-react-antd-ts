e0db85b68333e78e6b87a8f6635cac61d3ce3e00
hoantv <hoantv@cybertech.vn>
2024-03-04 17:57:47 +0700

    feat: đa ngôn ngữ

--- src/WrapApp.tsx
+++ src/WrapApp.tsx
@@ -7,12 +7,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AppProvider } from './context/app.context'
 import { ThemeProvider } from './context/theme.context'
-import viVNIntl from 'antd/lib/locale/vi_VN'
+import { useTranslation } from 'react-i18next'
+import { localesAntd } from './i18n/i18n'
 
 const WrapApp: React.FC = () => {
   const queryClient = new QueryClient({
     defaultOptions: { queries: { refetchOnWindowFocus: false } }
   })
+
+  const { i18n } = useTranslation()
+  const currentLanguageAntd = localesAntd[i18n.language as keyof typeof localesAntd]
 
   return (
     <ErrorBoundary>
@@ -26,7 +30,7 @@ const WrapApp: React.FC = () => {
                   cssVar: true,
                   hashed: false
                 }}
-                locale={viVNIntl}
+                locale={currentLanguageAntd}
               >
                 <App />
               </ConfigProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './reset.css'
-// import './index.css'
+import './index.css'
+import './i18n/i18n.ts'
 import WrapApp from './WrapApp.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
